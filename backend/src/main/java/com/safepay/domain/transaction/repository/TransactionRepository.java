1b1b6f94c670e846848c018e31d76a3a6071ac87
PSW99 <steamdayz@naver.com>
2026-03-15 21:44:09 +0900

    feat(transaction): findByIdempotencyKey 쿼리 메서드 추가
    - 멱등성 체크 시 accountId 없이 idempotencyKey만으로 조회할 수 있도록
    TransactionRepository에 findByIdempotencyKey() 추가

--- backend/src/main/java/com/safepay/domain/transaction/repository/TransactionRepository.java
+++ backend/src/main/java/com/safepay/domain/transaction/repository/TransactionRepository.java
@@ -12,4 +12,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     Page<Transaction> findByAccountIdOrderByCreatedAtDesc(Long accountId, Pageable pageable);
 
     Optional<Transaction> findByAccountIdAndIdempotencyKey(Long accountId, String idempotencyKey);
+
+    Optional<Transaction> findByIdempotencyKey(String idempotencyKey);
 }
